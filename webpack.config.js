73d7eb0d0de4c01bfabdf3218030ca73bc0c502c
Tepepeka <jeremy.bourgoin@live.fr>
2023-05-03 16:43:22 +0200

    webpack babel sass postcss minicss

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
   },
   module: {
     rules: [
       {
+        //JS
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
+          // Loader Babel
           loader: 'babel-loader',
           options: {
             presets: [
@@ -19,8 +23,42 @@ module.exports = {
             ]
           }
         }
-      }
+      },
+      {
+        // SASS + PostCSS
+        test:  /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          "postcss-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        // Images
+        test: /\.(png|jpeg|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        // Font
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
     ],
   },
+  plugins : [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    })
+  ],
+  watch:true,
   mode: 'development',
 };
